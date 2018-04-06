2a83a173865ea7bf6060864c4024ad8ed8db5683
Never <Never1750@163.com>
2018-04-06 13:57:46 +0800

    初始化项目

---
+++ ZBMacroDefine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZBMacroDefine
+//
+//  Created by Never on 2018/4/6.
+//  Copyright © 2018年 Never. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZBMacroDefine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZBMacroDefine
+//
+//  Created by Never on 2018/4/6.
+//  Copyright © 2018年 Never. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
